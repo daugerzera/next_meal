99ba64a44dc71ae35455a88ca303d2d2b2e5cca8
daugerzera <49286169+daugerzera@users.noreply.github.com>
2024-11-15 20:53:08 -0300

    git upload

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+
+'use client'
+
+export default function Error({ error }) {
+
+    return <main className="error">
+        <h1>Meal not found!</h1>
+        <p>We cannot find the requested meal!</p>
+    </main>
+}
---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+
+
+export default function NotFound({ error }) {
+
+    return <main className="not-found">
+        <h1>Not found</h1>
+        <p>Sorry, the page you are trying to reach does no exists!</p>
+    </main>
+}
